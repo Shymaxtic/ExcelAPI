33b864bc67d98e1d54b19182b139a3905464c762
shymaxtic <shymaxtic@gmail.com>
2019-10-28 20:51:59 +0700

    1. Add test case.

--- Test/ExcelApi_UT.py
+++ Test/ExcelApi_UT.py
@@ -47,5 +47,44 @@ class TestExcelApi(unittest.TestCase):
         ret = self.testFile.ReadRowByCondition({"Name":"Person1"})
         self.assertEqual(ret, expectedValue)
 
+    def test_LoadSheet(self):
+        print("--------------Load another sheet------------------")
+        self.testFile.LoadSheet("Sheet2", "C15:H16")          
+
+    def test_ReadAtRow2(self):
+        expectedValue = {"Name:":"Person1",
+        "Task:":"Task1", 
+        "Start Date:Coding:":"10/11",
+        "End Date:Coding:":"10/21",
+        "Start Date:Testing:":"11/11",
+        "End Date:Testing:":"11/21"}
+        ret = self.testFile.Read(1)
+        self.assertEqual(ret, expectedValue)
+
+    def test_ReadAtField2(self):
+        expectedValue = ["10/21","10/22","10/23","10/24","10/25","10/26"]
+        ret = self.testFile.ReadByField("End Date:Coding")
+        self.assertEqual(ret, expectedValue)
+        ret = self.testFile.ReadByField("End Date:Coding:")
+        self.assertEqual(ret, expectedValue)
+
+    def test_ReadByCondition2(self):
+        expectedValue = {"Task":["Task1", "Task2"], 
+        "Start Date:Coding":["10/11", "10/12"],
+        "Start Date:Testing":["11/11", "11/12"]}
+        ret = self.testFile.ReadByCondition(["Task", "Start Date:Coding", "Start Date:Testing"],
+        {"Name":"Person1"})
+        self.assertEqual(ret, expectedValue)
+
+    def test_ReadRowByCondition2(self):
+        expectedValue = {"Name:":["Person1", "Person1"],
+        "Task:":["Task1", "Task2"], 
+        "Start Date:Coding:":["10/11", "10/12"],
+        "End Date:Coding:":["10/21", "10/22"],
+        "Start Date:Testing:":["11/11", "11/12"],
+        "End Date:Testing:":["11/21", "11/22"]}
+        ret = self.testFile.ReadRowByCondition({"Name":"Person1"})
+        self.assertEqual(ret, expectedValue)              
+
 if __name__ == '__main__':
     unittest.main()        
